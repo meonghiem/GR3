9a978b3b0a188147ee91afa118be50b39f64bd33
meonghiem <meonghiem@gmail.com>
2024-04-19 01:35:31 +0700

    separate code

--- ARIMA.py
+++ ARIMA.py
@@ -22,45 +22,26 @@ from statsmodels.stats.descriptivestats import Description
 import matplotlib.pyplot as plt
 from numpy import log
 import numpy as np
+from utils import adf_test, check_normality
 
-def adf_test(timeseries):
-    print("Results of Dickey-Fuller Test:")
-    # dftest = adfuller(timeseries, autolag="AIC", regression="ctt", regresults=True)
-    dftest = adfuller(timeseries, autolag="AIC", regression="ct")
-    dfoutput = pd.Series(
-        dftest[0:4],
-        index=[
-            "Test Statistic",
-            "p-value",
-            "#Lags Used",
-            "Number of Observations Used",
-        ],
-    )
-    for key, value in dftest[4].items():
-        dfoutput["Critical Value (%s)" % key] = value
-    print(dfoutput)
-
-def check_normality(timeseries):
-    jb_value, p_value, skewness, kurtosis = jarque_bera(timeseries)
-    if p_value < 0.05:
-        print("Bác bỏ H0: chuỗi phân bố chuẩn => là chuỗi không tuân theo phân bố chuẩn")
-    else:
-        print("Không đủ điều kiện bác bỏ H0 => Chuỗi phân bố chuẩn")
 
 
 #Load data set
 series_influ_A_df = read_csv('vietnam_flu_A.csv')
 series_influ_A_df = series_influ_A_df.dropna()
+# Create Training and Test
+train = series_influ_A_df["Influenza A - All types of surveillance"][:700]
+test = series_influ_A_df["Influenza A - All types of surveillance"][700:]
 
 # basicStats = Description(data=series_influ_A_df["Influenza A - All types of surveillance"])
 # print(basicStats)
 
-adf_test(series_influ_A_df["Influenza A - All types of surveillance"])
+adf_test(train)
 # ở dicky-fuller thì H0: chuỗi không có tính dừng
 # mà p-value < 0.05 nên bác bỏ H0 => chuỗi có tính dừng
 # => d = 0
 
-check_normality(series_influ_A_df["Influenza A - All types of surveillance"])
+check_normality(train)
 # jb_value, p_value, skewness, kurtosis = jarque_bera(series_influ_A_df["Influenza A - All types of surveillance"])
 # print(p_value) # pvalue nhỏ  => bác bỏ H0: chuỗi phân bố chuẩn => là chuỗi không tuân theo phân bố chuẩn
 
@@ -72,10 +53,10 @@ import statsmodels.api as sm
 # plt.rcParams.update({'figure.figsize':(9,3), 'figure.dpi':120})
 
 fig, axes = plt.subplots(1, 3, figsize=(10,5))
-axes[0].plot(series_influ_A_df["Influenza A - All types of surveillance"]); axes[0].set_title('original')
+axes[0].plot(train); axes[0].set_title('original')
 # axes[1].set(ylim=(0,5))
-sm.graphics.tsa.plot_acf(series_influ_A_df["Influenza A - All types of surveillance"].dropna(), ax=axes[1], lags=10)
-sm.graphics.tsa.plot_pacf(series_influ_A_df["Influenza A - All types of surveillance"].dropna(), ax=axes[2], lags=10)
+sm.graphics.tsa.plot_acf(train.dropna(), ax=axes[1], lags=10)
+sm.graphics.tsa.plot_pacf(train.dropna(), ax=axes[2], lags=10)
 
 # plt.show()
 '''
@@ -102,44 +83,82 @@ Nhìn vào acf có thể thấy bậc của MA có thể là 1,2,3,4,5,6,7,8,9,1
 
 # How to build the ARIMA Model 
 
-import pmdarima as pm
+# import pmdarima as pm
 
-model = pm.auto_arima(series_influ_A_df["Influenza A - All types of surveillance"], start_p=1, start_q=1,
-                      test='adf',       # use adftest to find optimal 'd'
-                      max_p=3, max_q=10, # maximum p and q
-                      m=1,              # frequency of series
-                      d=None,           # let model determine 'd'
-                      seasonal=False,   # No Seasonality
-                      start_P=0, 
-                      D=0, 
-                      trace=True,
-                      error_action='ignore',  
-                      suppress_warnings=True, 
-                      stepwise=True)
+# model = pm.auto_arima(train, start_p=1, start_q=1,
+#                       test='adf',       # use adftest to find optimal 'd'
+#                       max_p=3, max_q=10, # maximum p and q
+#                       m=1,              # frequency of series
+#                       d=None,           # let model determine 'd'
+#                       seasonal=False,   # No Seasonality
+#                       start_P=0, 
+#                       D=0, 
+#                       trace=True,
+#                       error_action='ignore',  
+#                       suppress_warnings=True, 
+#                       stepwise=True)
+#chọn được p,d,q là 2,0,2
 
-print(model.summary())
+# print(model.summary())
 
-model.plot_diagnostics(figsize=(10,8))
+# model.plot_diagnostics(figsize=(10,8))
+# plt.show()
+
+
+# # Forecast
+# n_periods = 24
+# fc, confint = model.predict(n_periods=n_periods, return_conf_int=True)
+# index_of_fc = np.arange(len(train), len(train)+n_periods)
+
+# # make series for plotting purpose
+# fc_series = pd.Series(fc, index=index_of_fc)
+# lower_series = pd.Series(confint[:, 0], index=index_of_fc)
+# upper_series = pd.Series(confint[:, 1], index=index_of_fc)
+
+# # Plot
+# plt.plot(train)
+# plt.plot(fc_series, color='darkgreen')
+# plt.fill_between(lower_series.index, 
+#                  lower_series, 
+#                  upper_series, 
+#                  color='k', alpha=.15)
+
+# plt.title("Final Forecast of Usage")
+# plt.show()
+
+from statsmodels.tsa.arima.model import ARIMA
+model = ARIMA(train, order=(2,0,2))
+model_fit = model.fit()
+
+# Plot residual errors
+residuals = pd.DataFrame(model_fit.resid)
+fig, ax = plt.subplots(1,2)
+residuals.plot(title="Residuals", ax=ax[0])
+residuals.plot(kind='kde', title='Density', ax=ax[1])
 plt.show()
 
-
-# Forecast
-n_periods = 24
-fc, confint = model.predict(n_periods=n_periods, return_conf_int=True)
-index_of_fc = np.arange(len(series_influ_A_df["Influenza A - All types of surveillance"]), len(series_influ_A_df["Influenza A - All types of surveillance"])+n_periods)
-
-# make series for plotting purpose
-fc_series = pd.Series(fc, index=index_of_fc)
-lower_series = pd.Series(confint[:, 0], index=index_of_fc)
-upper_series = pd.Series(confint[:, 1], index=index_of_fc)
-
-# Plot
-plt.plot(series_influ_A_df["Influenza A - All types of surveillance"])
-plt.plot(fc_series, color='darkgreen')
-plt.fill_between(lower_series.index, 
-                 lower_series, 
-                 upper_series, 
-                 color='k', alpha=.15)
-
-plt.title("Final Forecast of Usage")
+# Actual vs Fitted
+from statsmodels.graphics.tsaplots import plot_predict
+fig, ax = plt.subplots(figsize=(10,8))
+ax.plot(train.loc[0:].reset_index(drop=True), '-g', label='observed')
+ax.plot(test.loc[0:].reset_index(drop=True), '-g', label='actual',color='darkgreen')
+plot_predict(model_fit, start=0, end=800, ax=ax)
 plt.show()
+
+# fc, se, conf = model_fit.forecast(119, alpha=0.05)  # 95% conf
+
+# # Make as pandas series
+# fc_series = pd.Series(fc, index=test.index)
+# lower_series = pd.Series(conf[:, 0], index=test.index)
+# upper_series = pd.Series(conf[:, 1], index=test.index)
+
+# # Plot
+# plt.figure(figsize=(12,5), dpi=100)
+# plt.plot(train, label='training')
+# plt.plot(test, label='actual')
+# plt.plot(fc_series, label='forecast')
+# plt.fill_between(lower_series.index, lower_series, upper_series, 
+#                  color='k', alpha=.15)
+# plt.title('Forecast vs Actuals')
+# plt.legend(loc='upper left', fontsize=8)
+# plt.show()
---
+++ utils.py
@@ -0,0 +1,53 @@
+from statsmodels.tsa.stattools import adfuller
+import pandas as pd
+from statsmodels.stats.stattools import jarque_bera
+import numpy as np
+
+from statsmodels.tsa.stattools import acf
+'''
+ở dicky-fuller thì H0: chuỗi không có tính dừng
+mà p-value < 0.05 nên bác bỏ H0 => chuỗi có tính dừng
+'''
+def adf_test(timeseries):
+    print("Results of Dickey-Fuller Test:")
+    # dftest = adfuller(timeseries, autolag="AIC", regression="ctt", regresults=True)
+    dftest = adfuller(timeseries, autolag="AIC", regression="ct")
+    dfoutput = pd.Series(
+        dftest[0:4],
+        index=[
+            "Test Statistic",
+            "p-value",
+            "#Lags Used",
+            "Number of Observations Used",
+        ],
+    )
+    for key, value in dftest[4].items():
+        dfoutput["Critical Value (%s)" % key] = value
+    print(dfoutput)
+
+# pvalue nhỏ  => bác bỏ H0: chuỗi phân bố chuẩn => là chuỗi không tuân theo phân bố chuẩn
+def check_normality(timeseries):
+    jb_value, p_value, skewness, kurtosis = jarque_bera(timeseries)
+    if p_value < 0.05:
+        print("Bác bỏ H0: chuỗi phân bố chuẩn => là chuỗi không tuân theo phân bố chuẩn")
+    else:
+        print("Không đủ điều kiện bác bỏ H0 => Chuỗi phân bố chuẩn")
+
+
+# Accuracy metrics
+def forecast_accuracy(forecast, actual, fc, test):
+    mape = np.mean(np.abs(forecast - actual)/np.abs(actual))  # MAPE
+    me = np.mean(forecast - actual)             # ME
+    mae = np.mean(np.abs(forecast - actual))    # MAE
+    mpe = np.mean((forecast - actual)/actual)   # MPE
+    rmse = np.mean((forecast - actual)**2)**.5  # RMSE
+    corr = np.corrcoef(forecast, actual)[0,1]   # corr
+    mins = np.amin(np.hstack([forecast[:,None], 
+                              actual[:,None]]), axis=1)
+    maxs = np.amax(np.hstack([forecast[:,None], 
+                              actual[:,None]]), axis=1)
+    minmax = 1 - np.mean(mins/maxs)             # minmax
+    acf1 = acf(fc-test)[1]                      # ACF1
+    return({'mape':mape, 'me':me, 'mae': mae, 
+            'mpe': mpe, 'rmse':rmse, 'acf1':acf1, 
+            'corr':corr, 'minmax':minmax})
